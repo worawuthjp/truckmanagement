784269251f5b5a94a7a76713627c0bfa8ae0b517
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2020-04-17 01:29:14 +0700

    update template adminlte

---
+++ web/resources/views/partials/footer.blade.php
@@ -0,0 +1,43 @@
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+<!-- jQuery -->
+<script src="{{asset('bootstrap/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('bootstrap/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="bootstrap/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="bootstrap/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="bootstrap/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="bootstrap/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="bootstrap/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="bootstrap/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="bootstrap/plugins/moment/moment.min.js"></script>
+<script src="bootstrap/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="bootstrap/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="bootstrap/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="bootstrap/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="bootstrap/dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="bootstrap/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="bootstrap/dist/js/demo.js"></script>
---
+++ web/resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+<link rel="stylesheet" href="{{asset('fontawesome/css/all.css')}}">
+<link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- Tempusdominus Bbootstrap 4 -->
+<link rel="stylesheet" href="{{asset('bootstrap/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{asset('bootstrap/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+<!-- JQVMap -->
+<link rel="stylesheet" href="{{asset('bootstrap/plugins/jqvmap/jqvmap.min.css')}}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{asset('bootstrap/dist/css/adminlte.min.css')}}">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{asset('bootstrap/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{asset('bootstrap/plugins/daterangepicker/daterangepicker.css')}}">
+<!-- summernote -->
+<link rel="stylesheet" href="{{asset('bootstrap/plugins/summernote/summernote-bs4.css')}}">
