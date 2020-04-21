2dd52cc73708b57599e38f9f7e6bcb8175cbdb69
ingPhanuphat <ingcho007@gmai.com>
2020-04-21 14:51:11 +0700

    profile and login

--- app/flutter_app/lib/main.dart
+++ app/flutter_app/lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutterapp/SecondPage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutterapp/loginPage.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'truck driver',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: LoginPage(),
     );
@@ -25,13 +23,122 @@ class MyApp extends StatelessWidget {
 class indexApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'truck driver',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: ()=>{
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage())
+          )
+        },
+        child: Text("ดูงานที่ได้รับ",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
       ),
-      home: MyHomePage(title: 'หน้าแรก'),
+    );
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "หน้าแรก ",
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
+        backgroundColor: Colors.blue[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('./assets/im1.png'),
+                ),
+                Text(
+                  'นาย เอ บีซีดี',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 25,
+                  ),
+                ),
+
+
+                SizedBox(
+                  height: 20.0,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+
+                Card(
+                    color: Colors.white,
+                    margin:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        '0895556666',
+                        style:
+                        TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+                      ),
+                    )),
+                Card(
+                    color: Colors.white,
+                    margin:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.location_on,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        '44/2 ต.กำแพงแสน อ.กำแพงแสน จ.นครปฐม',
+                        style:
+                        TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+                      ),
+                    )),
+
+                Card(
+                  color: Colors.white,
+                  margin:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.local_shipping,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'ทย 5666',
+                      style: TextStyle(fontSize: 20.0, fontFamily: 'Neucha'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -62,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               'ข้อมูล',
-              style: Theme.of(context).textTheme.headline5,
+              style: Theme.of(context).textTheme.headline,
             ),
             RaisedButton(
               onPressed: ()=>{
